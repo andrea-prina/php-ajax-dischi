a579b5c1b7ae009c19724dc5a447e2896ea86b6d
Andrea Prina <and.prina@gmail.com>
2022-08-25 15:39:39 +0200

    Create disk card structure

--- v1/index.php
+++ v1/index.php
@@ -4,9 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dischi PHP</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="../style.css">
+
 </head>
 <body>
-    
+    <header>
+
+    </header>
+
+    <main>
+        <div class="container d-flex justify-content-center flex-wrap">
+            <div class="disk-card text-center">
+                <img src="https://i.discogs.com/-G7kOMQ5xaYO0bhu6FnLr-SRjkP088PF9Fnbf3Skx4o/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM3MTE2/Mi0xNTMxNTE1NTc2/LTEwOTMuanBlZw.jpeg" alt="">
+                <h1>TITLE</h1>
+                <p>Author</p>
+                <p>Year</p>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
