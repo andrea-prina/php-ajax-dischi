c87cb40b8a788725e88868cd81014eca3f9cee55
Andrea Prina <and.prina@gmail.com>
2022-08-25 17:13:01 +0200

    Add genre to album data

--- database.php
+++ database.php
@@ -5,61 +5,71 @@
             "coverImgSrc" => "https://i.discogs.com/-G7kOMQ5xaYO0bhu6FnLr-SRjkP088PF9Fnbf3Skx4o/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM3MTE2/Mi0xNTMxNTE1NTc2/LTEwOTMuanBlZw.jpeg",
             "title" => "...And Justice for All",
             "author" => "Metallica",
-            "releaseYear" => "1988"
+            "releaseYear" => "1988",
+            "genre" => "Rock"
         ],
         [
             "coverImgSrc" => "https://m.media-amazon.com/images/I/A1q7irGXhoL._AC_SL1500_.jpg",
             "title" => "Fuori Dall'Hype Ringo Starr",
             "author" => "Pinguini Tattici Nucleari",
-            "releaseYear" => "2020"
+            "releaseYear" => "2020",
+            "genre" => "Pop"
         ],
         [
             "coverImgSrc" => "https://legendarycover.it/wp-content/uploads/2020/11/mr-simpatia.jpg",
             "title" => "Mr. Simpatia",
             "author" => "Fabri Fibra",
-            "releaseYear" => "2004"
+            "releaseYear" => "2004",
+            "genre" => "Rap"
         ],
         [
             "coverImgSrc" => "https://upload.wikimedia.org/wikipedia/en/a/a7/Random_Access_Memories.jpg",
             "title" => "Random Access Memory",
             "author" => "Daft Punk",
-            "releaseYear" => "2013"
+            "releaseYear" => "2013",
+            "genre" => "Electro"
         ],
         [
             "coverImgSrc" => "https://pure-music.co.uk/wp-content/uploads/2019/11/the-wall-album-cover-355x321.jpg",
             "title" => "The Wall",
             "author" => "Pink Floyd",
-            "releaseYear" => "1979"
+            "releaseYear" => "1979",
+            "genre" => "Rock"
         ],
         [
             "coverImgSrc" => "https://www.newsic.it/wp-content/uploads/2019/10/MARRA_cover-PERSONA_b-786x786.jpg",
             "title" => "Persona",
             "author" => "Marracash",
-            "releaseYear" => "2019"
+            "releaseYear" => "2019",
+            "genre" => "Rap"
         ],
         [
             "coverImgSrc" => "https://upload.wikimedia.org/wikipedia/en/a/a8/Creamwheelsoffire.jpeg",
             "title" => "Wheels of Fire",
             "author" => "Cream",
-            "releaseYear" => "1968"
+            "releaseYear" => "1968",
+            "genre" => "Rock"
         ],
         [
             "coverImgSrc" => "https://upload.wikimedia.org/wikipedia/en/a/a4/Pink_Floyd%2C_Wish_You_Were_Here_%281975%29.png",
             "title" => "Wish You Were Here",
             "author" => "Pink Floyd",
-            "releaseYear" => "1975"
+            "releaseYear" => "1975",
+            "genre" => "Rock"
         ],
         [
             "coverImgSrc" => "https://upload.wikimedia.org/wikipedia/en/4/45/Billie_Eilish_-_Happier_Than_Ever.png",
             "title" => "Happier Than Ever",
             "author" => "Billie Eilish",
-            "releaseYear" => "2021"
+            "releaseYear" => "2021",
+            "genre" => "Pop"
         ],
         [
             "coverImgSrc" => "https://miro.medium.com/focal/1200/1200/50/40/1*8FkvzbSdSJ4HNxtuZo5kLg.jpeg",
             "title" => "The Dark Side of the Moon",
             "author" => "Pink Floyd",
-            "releaseYear" => "1973"
+            "releaseYear" => "1973",
+            "genre" => "Rock"
         ],  
     ];
 
