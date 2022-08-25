e17c1ddc1bbbc33d22db533c7868991d07623329
Andrea Prina <and.prina@gmail.com>
2022-08-25 17:28:54 +0200

    Add genre filter to the API call

--- v2/controller.php
+++ v2/controller.php
@@ -2,8 +2,23 @@
 
     include("../database.php");
 
+    $filteredDisks = [];
+
+    if (!empty($_GET["genre"])){
+
+        foreach ($disks as $disk) {
+            if ($disk["genre"] === $_GET["genre"]){
+                $filteredDisks[] = $disk;
+            }
+        };
+
+    } else {
+
+        $filteredDisks = $disks;
+    };
+
     header('Content-type: application/json');
 
-    echo json_encode($disks);
+    echo json_encode($filteredDisks);
 
 ?>
